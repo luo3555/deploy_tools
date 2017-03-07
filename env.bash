aa991248f0ca6b387716b7efb59ea5761321339e
Daniel <daniel3555@gmail.com>
2017-03-07 14:03:06 +0800

    update"

--- env.bash
+++ env.bash
@@ -2,15 +2,15 @@
 source /opt/data/tools/.git-completion.bash
 PS1='\[\033[0m\]\u:\[\033[5;37m\]\w\[\033[1;32m\]$(__git_ps1 " (%s)")\[\033[0m\] $ \[\033[0m\]'
 
+#custome alias
+alias cdsmdecc='cd /var/www/html/5.6/local.smd-ecc.com'
+alias cdapi='cd /var/www/html/5.6/local.api.com'
+alias cdcamorama='cd /var/www/html/5.6/local.camorama.com'
+alias cdhonghua='cd /var/www/html/5.6/local.honghua.com'
+alias cdhollywoodsuits='cd /var/www/html/7.0.16/local.hollywood.com'
+
+
 C_TOOLS=/opt/data/tools
-C_BIN=/opt/data/usr/bin
-C_LIB=/opt/data/usr/lib
-C_INCLUDE=/opt/data/usr/include
-C_PHP=/opt/data/php/use/bin
-C_MYSQL=/opt/data/mysql/use/bin
-C_APACHE=/opt/data/apache/use/bin
 C_NODEJS=/opt/data/nodejs/use/bin
-C_NPM=/opt/data/npm/use/bin
-C_DEPOT_TOOLS=/opt/data/depot_tools
-C_PATH=$C_TOOLS:$C_BIN:$C_LIB:$C_INCLUDE:$C_PHP:$C_MYSQL:$C_APACHE:$C_DRUPAL_CONSOLE:$C_NODEJS:$C_NPM:$C_DEPOT_TOOLS
+C_PATH=$C_TOOLS:$C_NODEJS
 export PATH=$C_PATH:$PATH
